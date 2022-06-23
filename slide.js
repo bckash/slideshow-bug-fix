5684c6db804933b4ebe96ac49a519e865f98006e
bart c <kaszikasz@wp.pl>
2022-06-23 23:54:49 +0100

    minimal version for example

--- slide.js
+++ slide.js
@@ -1,8 +1,8 @@
 
 // const
 
-const div1 = document.getElementById("d1")
-const div2 = document.getElementById("d2")
+const div1 = document.getElementById("d1"),
+div2 = document.getElementById("d2")
 
 const li1 = document.getElementById("li1")
 const li2 = document.getElementById("li2")
@@ -12,41 +12,45 @@ let index = 0;
 // EL
 
 li1.addEventListener("click", slideShowAbstract)
+li2.addEventListener("click", slideShowAbstract)
 
 // function
 
+let y;
 
+function slideShowAbstract(e) {
 
-function slideShowAbstract() {
+    // function addRemove (anim) {
+    //     setTimeout(() => {
+    //         div1.classList.add(anim)
+    //     }, 0)
 
-    index += 1;
-    console.log(index)
-
-    function addRemove (anim) {
-        setTimeout(() => {
-            div1.classList.add(anim)
-        }, 0)
-
-        setTimeout(() => {
-            div2.classList.remove(anim)
-        }, 1000)
+    //     setTimeout(() => {
+    //         div2.classList.remove(anim)
+    //     }, 1000)
     
-        setTimeout(() => {
-            div2.classList.add(anim)
-        }, 3000)
+    //     setTimeout(() => {
+    //         div2.classList.add(anim)
+    //     }, 3000)
 
-        setTimeout(() => {
-            div1.classList.remove(anim)
-        }, 4000)
-    }
+    //     setTimeout(() => {
+    //         div1.classList.remove(anim)
+    //     }, 4000)
+    // }
 
-    if (index === 1) {
-        const x = setTimeout(()=> addRemove("animation__full"), 0)
-        const y = setInterval(()=> addRemove("animation__full"), 6000)
-    } else if (index === 3) {
-        index = 2
+    if (e.target.id === "li1") {
+        y = setInterval(()=> console.log("playing"), 2000)
     }
 
 
 
-}
+
+    if (e.target.id === "li2") {
+
+            console.log("stop")
+            clearInterval(y)
+
+        }
+    } 
+
+

321c8ee0cb8ac0f756863f92dedbdad86a6d8003
bart c <kaszikasz@wp.pl>
2022-06-24 18:51:04 +0100

    working setup for zer00ne

--- slide.js
+++ slide.js
@@ -1,56 +1,43 @@
 
 // const
 
-const div1 = document.getElementById("d1"),
-div2 = document.getElementById("d2")
+const
+    div1 = document.getElementById("d1"),
+    div2 = document.getElementById("d2"),
 
-const li1 = document.getElementById("li1")
-const li2 = document.getElementById("li2")
+    li1 = document.getElementById("li1"),
+    li2 = document.getElementById("li2");
 
-let index = 0;
+let 
+    index = 0,
+    y,
+    t = 1000,
+    tick = 0;
+
+const 
+    log = () => console.log(t * tick++);
 
 // EL
 
-li1.addEventListener("click", slideShowAbstract)
-li2.addEventListener("click", slideShowAbstract)
+
+li1.addEventListener("click", start)
+li2.addEventListener("click", stop)
+
 
 // function
 
-let y;
 
-function slideShowAbstract(e) {
+function start(e) {
+  if (e.target.matches('#li1') && !y) {
+    y = setInterval(log, t);
+  }
+}
 
-    // function addRemove (anim) {
-    //     setTimeout(() => {
-    //         div1.classList.add(anim)
-    //     }, 0)
-
-    //     setTimeout(() => {
-    //         div2.classList.remove(anim)
-    //     }, 1000)
-    
-    //     setTimeout(() => {
-    //         div2.classList.add(anim)
-    //     }, 3000)
-
-    //     setTimeout(() => {
-    //         div1.classList.remove(anim)
-    //     }, 4000)
-    // }
-
-    if (e.target.id === "li1") {
-        y = setInterval(()=> console.log("playing"), 2000)
+function stop(e) {
+    if (e.target.matches('#li2')) {
+      clearInterval(y);
+      y = null;
     }
-
-
-
-
-    if (e.target.id === "li2") {
-
-            console.log("stop")
-            clearInterval(y)
-
-        }
-    } 
+  }
 
 
